f67abe644e9dffdaad41d7fa0a4caf7f8ce226e6
rumeysa-akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-03-01 09:59:30 +0300

    Add project files.

---
+++ Core/GymManagement.Domain/Entities/Trainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GymManagement.Domain.Entities
+{
+    public class Trainer:Worker
+    {
+        public List<Member> Members { get; set; }
+        
+    }
+}
